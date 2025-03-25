b7244e12397396a307df753da598e50ead5ba382
DaYundexTuxi <nr00575@rvt.lv>
2025-03-25 19:03:19 +0200

    Returned the work for a day. still in progress, might add a bit more comments tmrw.

---
+++ Message-project/MessageGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Message_project
+{
+    class MessageGenerator // class only for generating messages as text to send them
+    {
+        // construction parts for the messages 
+        // 
+        string[] themesArray = { "Birthday", "InternationalWomensDay", "NewYear" };
+
+        // arrays of simple greetings 
+        string[] formalGreetingsArray = { "Hello", "Good day", "" }; // 
+        string[] informalGreetingsArray = { "Heya", "What\'s up?", "Hi" };
+
+        string messageText;
+        Random random = new();
+
+        // possible for using the constructor, but there's a problem with calling methods
+        /*
+        public MessageConstructor()
+        {
+            
+        }
+        */
+
+        public string generateMessageText(string theme) //!!!!!!!!!!!!!!!!
+        {
+            switch (theme)
+            {
+                case "Birthday":
+                    messageText = $"{formalGreetingsArray[random.NextInt64(0, formalGreetingsArray.Length)]}, I wish you hapiness and growing income!\n Best regards, your dear friend!!";
+                    break;
+                case "InternationalWomansDay":
+                    messageText = $"{formalGreetingsArray[random.NextInt64(0, formalGreetingsArray.Length)]}, have a lovely day and \n Best regards, your dear friend!!";
+                    break;
+                case "NewYear":
+                    messageText = $"{formalGreetingsArray[random.NextInt64(0, formalGreetingsArray.Length)]}, I wish you hapiness and growing income!\n Best regards, your dear friend!!";
+                    break; 
+            }
+            return messageText;
+        }
+    }
+}
--- Message-project/Program.cs
+++ Message-project/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Aboba");
+﻿using System.Transactions; // not sure how to use this 
+using Message_project;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] enteredPhoneNumberArray = { };
+        bool processOfEnteringNumbers = true; // bool for while loop: to input numbers in to the enteredPhoneNumberArray
+
+
+        // start of the program
+        Console.WriteLine("Enter the desired phone numbers: ");
+        Console.WriteLine("You can enter \'exit\' to exit the entering the numbers");
+
+        // loop for filling the array with phone numbers
+        while (processOfEnteringNumbers)
+        {
+            string inputedText = Console.ReadLine();
+
+            if (inputedText == "exit")
+            {
+                break;
+            }
+            else if (PhoneNumber.isValid(inputedText))
+            {
+                enteredPhoneNumberArray = new string[] { inputedText };
+            }
+            else
+            {
+                Console.WriteLine("Invalid number entered, try again.");
+            }
+
+            //switch (input)
+            //{
+            //    case "exit":
+            //        enteringNumbers = false;
+            //        break;
+            //    case :
+            //        Console.WriteLine("a");
+            //    default: 
+            //}
+        }
+
+        // showing the list of entered phone numbers
+        foreach (string pNumber in enteredPhoneNumberArray)
+        {
+            Console.WriteLine($"{pNumber} is in the list.");
+        }
+
+        //asking for sending email + should be 
+        Console.Write($"You have entered {enteredPhoneNumberArray.Length} phone numbers, do you want to send them an sms?(y/n): ");
+        char answerAfterPNumbers = Convert.ToChar(Console.ReadLine());
+
+        if (answerAfterPNumbers == 'y')
+        {
+            foreach (string pn in enteredPhoneNumberArray)
+            {
+                PhoneNumber usedPhoneNumber = new PhoneNumber(pn);
+                usedPhoneNumber.;
+            }
+        }
+        else
+        {
+            Console.WriteLine("ok-");
+        }
+    }
+}
