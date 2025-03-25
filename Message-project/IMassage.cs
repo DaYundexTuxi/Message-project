93f7338f9e4fa0423194774219b9864982949a58
DaYundexTuxi <nr00575@rvt.lv>
2025-03-25 18:05:52 +0200

    Got some work done, WIP.

---
+++ Message-project/IMassage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Message_project
+{
+    internal interface IMassage // Interface  to further inherit the basics of the message such as the text, theme, etc. in other classes
+    {
+        string Theme { get; set; }
+        string MessageText { get; set; }
+
+        
+    }
+}
